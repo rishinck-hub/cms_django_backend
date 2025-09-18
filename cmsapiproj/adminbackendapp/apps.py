e7a3fea3d6a305dc399964c979b70b6af8608d3e
rishinck-hub <rishinkpm@gmail.com>
2025-09-18 11:04:22 +0530

    first ccommit

---
+++ cmsapiproj/adminbackendapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdminbackendappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'adminbackendapp'
---
+++ cmsapiproj/doctorbackendapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DoctorbackendappendappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'doctorbackendappendapp'
